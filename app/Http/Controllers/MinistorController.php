1c0d78943b70eae06944d8576d76cd1800dc6c8b
oum burs <oum.burs9mry@gmail.com>
2023-02-21 10:30:14 +0300

    Tables created for each

---
+++ app/Http/Controllers/MinistorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ministor;
+use Illuminate\Http\Request;
+
+class MinistorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ministor  $ministor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ministor $ministor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ministor  $ministor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ministor $ministor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ministor  $ministor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ministor $ministor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ministor  $ministor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ministor $ministor)
+    {
+        //
+    }
+}
